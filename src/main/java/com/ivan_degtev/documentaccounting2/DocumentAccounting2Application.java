7038e1a11d38305c3a6fe5c66c03ad3a5e8dc3d7
ivan <diogteff.ivan@yandex.ru>
2024-07-13 10:01:01 +0300

    added checkstyle configeration

--- src/main/java/com/ivan_degtev/documentaccounting2/DocumentAccounting2Application.java
+++ src/main/java/com/ivan_degtev/documentaccounting2/DocumentAccounting2Application.java
@@ -2,19 +2,13 @@ package com.ivan_degtev.documentaccounting2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class DocumentAccounting2Application {
-
 	public static void main(String[] args) {
 		System.setProperty("spring.devtools.restart.enabled", "true");
 		SpringApplication.run(DocumentAccounting2Application.class, args);
 	}
-
 }
