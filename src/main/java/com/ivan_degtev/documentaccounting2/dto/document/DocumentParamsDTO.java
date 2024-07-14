76c7b2c472617203d9fbba2e68f5ecefc4baacee
ivan <diogteff.ivan@yandex.ru>
2024-07-14 20:46:28 +0300

    I made the search take into account the fields of document publicity and user accessibility

--- src/main/java/com/ivan_degtev/documentaccounting2/dto/document/DocumentParamsDTO.java
+++ src/main/java/com/ivan_degtev/documentaccounting2/dto/document/DocumentParamsDTO.java
@@ -1,16 +1,21 @@
 package com.ivan_degtev.documentaccounting2.dto.document;
 
+import com.ivan_degtev.documentaccounting2.utils.UserUtils;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 @Getter
 @Setter
 @ToString
 public class DocumentParamsDTO {
+
     private String titleCont;
     private Long number;
     private String authorCont;
@@ -19,6 +24,8 @@ public class DocumentParamsDTO {
 
     @DateTimeFormat(pattern = "yyyy-MM-dd") // Указываем формат даты
     private LocalDate creationDate;
+
+    private Boolean publicDocument = true;
 
     // Для сортировки
     private String sortBy; // поле, по которому нужно сортировать ('title', 'author', ...)
--- src/main/java/com/ivan_degtev/documentaccounting2/specification/DocumentSpecification.java
+++ src/main/java/com/ivan_degtev/documentaccounting2/specification/DocumentSpecification.java
@@ -2,6 +2,17 @@ package com.ivan_degtev.documentaccounting2.specification;
 
 import com.ivan_degtev.documentaccounting2.dto.document.DocumentParamsDTO;
 import com.ivan_degtev.documentaccounting2.model.Document;
+import com.ivan_degtev.documentaccounting2.model.User;
+import com.ivan_degtev.documentaccounting2.service.impl.DocumentServiceImpl;
+import com.ivan_degtev.documentaccounting2.utils.UserUtils;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Root;
+import jakarta.persistence.criteria.Subquery;
+import lombok.AllArgsConstructor;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
@@ -9,14 +20,22 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 
 @Component
+@AllArgsConstructor
 public class DocumentSpecification {
+
+    private UserUtils userUtils;
+    private static final Logger logger = LoggerFactory.getLogger(DocumentSpecification.class);
+
     public Specification build(DocumentParamsDTO params) {
+        Long idCurrentUser = userUtils.getCurrentUser().getIdUser();
         return withTitleCount(params.getTitleCont())
                 .and(withAuthorCount(params.getAuthorCont()))
                 .and(withContentCount(params.getContentCont()))
                 .and(withTypeCount(params.getTypeCont()))
                 .and(withNumber(params.getNumber()))
-                .and(withCreatedDate(params.getCreationDate()));
+                .and(withCreatedDate(params.getCreationDate()))
+//                .and(withPublicDocument(params.getPublicDocument()))
+                .and(withAvailableFor(idCurrentUser));
     }
 
     private Specification<Document> withTitleCount(String titleCont) {
@@ -54,6 +73,40 @@ public class DocumentSpecification {
                 ? cb.conjunction()
                 : cb.equal(root.get("creationDate"), date);
     }
+//    private Specification<Document> withPublicDocument(Boolean publicDocument) {
+//        return (root, query, cb) -> publicDocument == null
+//                ? cb.conjunction()
+//                : cb.equal(root.get("publicDocument"), publicDocument);
+//    }
+
+    public static Specification<Document> withAvailableFor(Long availableForUserId) {
+        return (root, query, cb) -> {
+            if (availableForUserId == null) {
+                return cb.conjunction();
+            }
+
+            // Создание подзапроса для проверки наличия пользователя в availableFor
+            Subquery<Long> subquery = query.subquery(Long.class);
+            logger.info("подзапрос {}", subquery);
+            Root<Document> subRoot = subquery.from(Document.class);
+            logger.info("сабрут {}", subRoot);
+            Join<Document, User> subJoin = subRoot.join("availableFor", JoinType.LEFT);
+            logger.info("ссоедение таблицы документа по полю доступности для {}", subJoin);
+            subquery.select(subRoot.get("id"))
+                    .where(
+                            cb.equal(subJoin.get("idUser"), availableForUserId),
+                            cb.equal(subRoot.get("id"), root.get("id"))
+                    );
+
+            return cb.or(
+                    cb.equal(root.get("publicDocument"), true),
+                    cb.equal(root.get("author").get("idUser"), availableForUserId),
+                    cb.exists(subquery)
+            );
+        };
+    }
+
+
     public Sort createSort(DocumentParamsDTO params) {
         Sort sort = Sort.by("title").ascending(); // сортировка по умолчанию
         if (params.getSortBy() != null && !params.getSortBy().isEmpty()) {
